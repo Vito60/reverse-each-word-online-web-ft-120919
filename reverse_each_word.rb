2e7a10f436ab9ac831ceb7b31aaf2b7af7048f1f
Randall Williams <randallw1996@gmail.com>
2019-12-11 22:14:15 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(string)
 new_array = string.split(" ")
 #reversed_array = []
   new_array.collect do |word|
-    word.reverse
+    "#{word.reverse}"
     #reversed_array << word.reverse
   end
  #return reversed_array.join(" ")
